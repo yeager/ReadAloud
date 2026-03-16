67a9a848ad220364c9f8192a8da2b92bf184ed5c
Daniel Nylander <daniel@danielnylander.se>
2026-03-16 16:11:19 +0100

    i18n: proper locale/gettext setup with LANG support

--- readaloud/i18n.py
+++ readaloud/i18n.py
@@ -1,20 +1,30 @@
-"""Internationalization support using gettext."""
-
 import gettext
 import locale
 import os
 
 DOMAIN = "readaloud"
-LOCALE_DIR = os.path.join(os.path.dirname(__file__), "..", "locale")
+LOCALEDIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "locale")
 
-# Try to set Swedish locale, fall back to system default
-for loc in ("sv_SE.UTF-8", "sv_SE", ""):
-    try:
-        locale.setlocale(locale.LC_ALL, loc)
+# System locale dirs
+SYSTEM_LOCALEDIRS = [
+    LOCALEDIR,
+    "/usr/share/locale",
+    "/usr/local/share/locale",
+]
+
+try:
+    locale.setlocale(locale.LC_ALL, "")
+except locale.Error:
+    pass
+
+# Find locale dir with our translations
+for d in SYSTEM_LOCALEDIRS:
+    if os.path.isdir(d):
+        gettext.bindtextdomain(DOMAIN, d)
         break
-    except locale.Error:
-        continue
+else:
+    gettext.bindtextdomain(DOMAIN, LOCALEDIR)
 
-_translation = gettext.translation(DOMAIN, LOCALE_DIR, fallback=True)
-_ = _translation.gettext
-ngettext = _translation.ngettext
+gettext.textdomain(DOMAIN)
+_ = gettext.gettext
+
